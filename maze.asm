f07a334fe39451dc35d8f1322de99608a8ed2e2b
Sedat Kapanoglu <ssg@users.noreply.github.com>
2018-12-29 18:30:35 -0800

    improve random generation

--- maze.asm
+++ maze.asm
@@ -1,13 +1,18 @@
 org         &4000
-char        equ &cc
+write direct "maze.bin"
+
+CHAR        equ &cc
 TXT_WR_CHAR equ &bb5d
 
 run $
 
 .loop:  
   ld    a, r
+  rrca
+  rrca
+  rrca
   and   1
-  or    char  
+  or    CHAR
   call  TXT_WR_CHAR  
-  jr    loop
-  
+  jr    loop  
+
